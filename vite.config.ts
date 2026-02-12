1b8acc595a1f0047c3fe16da4596626d7fb5678a
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-02-13 02:45:33 +0530

    fix change to 'vite.config.ts' file

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base:process.env.VITE_BASE_PATH || "/issue-tracker-frontend",
-  base:process.env.VITE_BASE_PATH,
+  base:process.env.VITE_BASE_PATH || "/issue-tracker-frontend",
+  // base:process.env.VITE_BASE_PATH,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
